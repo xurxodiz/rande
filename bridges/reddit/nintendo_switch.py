e374d3dce17659d929df01cf6eaef62e133a7875
Jorge Diz Pico <xurxo@xurxodiz.eu>
2024-02-11 17:59:44 +0100

    Simplify reddit fetching modules

--- bridges/reddit/nintendo_switch.py
+++ bridges/reddit/nintendo_switch.py
@@ -1,38 +1,13 @@
-from datetime import datetime
-import html
-
 from .reddit_feed import RedditFeed
 
 class NintendoSwitch(RedditFeed):
 
   def __init__(self):
-    super().__init__("NintendoSwitch")
-
-  def feed_data(self):
-
     data = {
       "title": "Reddit - Nintendo Switch",
       "link": "https://reddit.com/r/NintendoSwitch/",
       "description": "The central hub for all news, updates, rumors, and topics relating to the Nintendo Switch. We are a fan-run community, not an official Nintendo forum.",
       "itemlist": []
     }
+    super().__init__("NintendoSwitch", data)
 
-    for pt in self.js["data"]["children"]:
-
-      post = pt["data"]
-      item = {}
-
-      item["pubDate"] = datetime.fromtimestamp(post['created'])
-
-      item["title"] = post['title']
-      item["link"] = f"https://reddit.com{post['permalink']}"
-
-      if post["selftext_html"]:
-        item["description"] = html.unescape(post['selftext_html'])
-
-      if post["thumbnail"] != "self":
-        item["media_thumbnail"] = post['thumbnail']
-
-      data["itemlist"].append(item)
-
-    return data
--- bridges/reddit/reddit_feed.py
+++ bridges/reddit/reddit_feed.py
@@ -1,13 +1,34 @@
-from abc import ABC, abstractmethod
+from datetime import datetime
+import html
 import requests
 
-class RedditFeed(ABC):
+class RedditFeed():
 
-  def __init__(self, subreddit):
+  def __init__(self, subreddit, data):
     response = requests.get(f'https://www.reddit.com/r/{subreddit}.json')
     response.raise_for_status() # if any
     self.js = response.json()
+    self.data = data
 
-  @abstractmethod
   def feed_data(self):
-    pass
+
+    for pt in self.js["data"]["children"]:
+
+      post = pt["data"]
+      item = {}
+
+      item["pubDate"] = datetime.fromtimestamp(post['created'])
+
+      item["title"] = post['title']
+      item["link"] = f"https://reddit.com{post['permalink']}"
+
+      if post["selftext_html"]:
+        item["description"] = html.unescape(post['selftext_html'])
+
+      if post["thumbnail"] != "self":
+        item["media_thumbnail"] = post['thumbnail']
+
+      self.data["itemlist"].append(item)
+
+    return self.data
+
